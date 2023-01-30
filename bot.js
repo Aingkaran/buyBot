3b0c35389f0271c99dbfbe98b09115f14bce965f
Aingkaran <aingkaran1995@gmail.com>
2023-01-29 22:25:24 -0500

    add to cart function

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const puppeteer = require("puppeteer")
 console.log('hello')
-const product_url= "https://www.walmart.com/ip/ZINGYOU-BM800-Condenser-Microphone-Bundle-Mic-Kit-for-Studio-Recording-Brocasting/643118590?athbdg=L1600"
+const product_url= "https://www.nike.com/ca/t/air-jordan-1-mid-shoes-f8W9ns/DQ8426-517"
 
 const givePage=async ()=>{
     const browser = await puppeteer.launch({headless: false});
@@ -10,8 +10,18 @@ const givePage=async ()=>{
 
 const addToCart= async (page)=>{
     await page.goto(product_url);
-    await page.waitForSelector("button[class='w_hhLG w_8nsR w_jDfj']");
-    await page.click("button[class='w_hhLG w_8nsR w_jDfj']", elem => elem.click());
+    await page.waitForSelector("label[for='skuAndSize__28644620']")
+    await page.click("label[for='skuAndSize__28644620']", elem =()=>{
+        elem.click()
+        console.log("clicked")
+    } );
+
+    await page.waitForSelector("button[class='ncss-btn-primary-dark btn-lg add-to-cart-btn']");
+    await page.click("button[class='ncss-btn-primary-dark btn-lg add-to-cart-btn']", elem =()=>{
+        elem.click()
+        console.log("clicked")
+    } );
+
 }
 
 const checkout= async()=>{
