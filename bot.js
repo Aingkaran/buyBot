f4815f89aaefef8d279598cd8fbc462ab24e0c26
Aingkaran <aingkaran1995@gmail.com>
2023-02-01 18:07:50 -0500

    new site change

--- bot.js
+++ bot.js
@@ -1,26 +1,25 @@
 const puppeteer = require("puppeteer")
 console.log('hello')
-const product_url= "https://www.nike.com/ca/t/air-jordan-1-mid-shoes-f8W9ns/DQ8426-517"
+const product_url = "https://takomogolf.com/iron-101/"
 
-const givePage=async ()=>{
-    const browser = await puppeteer.launch({headless: false});
-    const page= await browser.newPage();
+const givePage = async () => {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
     return page;
 }
 
-const addToCart= async (page)=>{
+const addToCart = async (page) => {
     await page.goto(product_url);
-    await page.waitForSelector("label[for='skuAndSize__28644620']")
-    await page.click("label[for='skuAndSize__28644620']", elem=>elem.click())
-
-    await page.waitForSelector("button[class='ncss-btn-primary-dark btn-lg add-to-cart-btn']");
-    await page.evaluate(()=>document.getElementsByClassName('ncss-btn-primary-dark btn-lg add-to-cart-btn')[0].click())
-    await page.waitForSelector("button[class='ncss-btn-primary-dark btn-lg mr3-sm css-1n4ymyz']");
-    await page.evaluate(()=>document.getElementsByClassName('ncss-btn-primary-dark btn-lg mr3-sm css-1n4ymyz')[0].click());
+    await page.evaluate(() => document.querySelector('#hand option[value="Left"]').selected = true);
+    await page.evaluate(() => document.querySelector('#shaft option[value="KBS Max Stiff"]').selected = true);
+    await page.waitForTimeout(2000);
+    await page.waitForSelector("button[class='single_add_to_cart_button button alt']");
+    await page.evaluate(() => document.getElementsByClassName('single_add_to_cart_button button alt')[0].click());
+    await page.waitForNavigation();
 
 }
 
-const checkout= async()=>{
+const checkout = async () => {
     let page = await givePage();
     addToCart(page);
 }
